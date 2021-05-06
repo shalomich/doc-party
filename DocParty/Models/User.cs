f5e2e2ce56fec3e7c0a272e8ad78efa1e6cd7258
shalomich <shalo6947@gmail.com>
2021-05-06 16:56:16 +0700

    create class User

---
+++ DocParty/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Models
+{
+    class User : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
