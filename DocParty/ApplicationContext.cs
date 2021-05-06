e3ea9d2859143198b1e346ec8b0a21393dca18c1
shalomich <shalo6947@gmail.com>
2021-05-06 18:52:45 +0700

    create class ApplicationContext

---
+++ DocParty/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using DocParty.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty
+{
+    class ApplicationContext : IdentityDbContext<User, IdentityRole<int>, int,
+        IdentityUserClaim<int>, ProjectUserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+    }
+}
--- DocParty/Models/User.cs
+++ DocParty/Models/User.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DocParty.Models
 {
-    class User : IEntity
+    class User : IdentityUser<int>, IEntity
     {
-        public int Id { set; get; }
+        public override int Id { set; get; }
         public IEnumerable<Project> Projects { set; get; }
     }
 }
