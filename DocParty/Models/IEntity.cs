0a5d369ea0f34dce7f5f5889f245ad571386ecef
shalomich <shalo6947@gmail.com>
2021-05-06 16:55:14 +0700

    create interface IEntity

---
+++ DocParty/Models/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Models
+{
+    interface IEntity
+    {
+        public int Id { set; get; }
+    }
+}
