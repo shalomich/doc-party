344b48627ea373bef4899036c970325b583c63fb
shalomich <shalo6947@gmail.com>
2021-05-08 17:43:18 +0700

    create class LoginHandler

---
+++ DocParty/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using DocParty.Handlers.Account.Login;
+using DocParty.Handlers.Account.Register;
+using DocParty.RequestHandlers;
+using DocParty.RequestHandlers.Logout;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content($"{User.Identity.Name} is authorized");
+        }
+
+        public IActionResult Login()
+        {
+            return Content($"Need to authorize");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginQuery request)
+        {
+            return await AuthRequestHandle(request);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegistrationCommand request)
+        {
+            return await AuthRequestHandle(request);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogoutAsync()
+        {
+            await _mediator.Send(new LogoutQuery());
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<IActionResult> AuthRequestHandle(IRequest<ErrorResponce> request)
+        {
+            if (ModelState.IsValid)
+            {
+                ErrorResponce responce = await _mediator.Send(request);
+
+                if (responce.Errors.Any() == false)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    foreach (string message in responce.Errors)
+                        ModelState.AddModelError("", message);
+                }
+            }
+
+            return new JsonResult(ModelState);
+        }
+    }
+}
---
+++ DocParty/RequestHandlers/Account/Login/LoginHandler.cs
@@ -0,0 +1,49 @@
+﻿using DocParty.Models;
+using DocParty.RequestHandlers;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.Account.Login
+{
+    class LoginHandler : IRequestHandler<LoginQuery, ErrorResponce>
+    {
+        private const string InvalidEmailMessage = "This email is not registered";
+        private const string InvalidPasswordMessage = "This password is wrong for current email";
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public LoginHandler(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+
+        public async Task<ErrorResponce> Handle(LoginQuery request, CancellationToken cancellationToken)
+        {
+            var errors = new List<string>();
+
+			User user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                errors.Add(InvalidEmailMessage);
+                return new ErrorResponce(errors);
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+            }
+            else errors.Add(InvalidPasswordMessage);
+            
+            return new ErrorResponce(errors);
+		}
+	}
+}
