ebe1f26b84b2d2353f42e2408504a4be07a4c552
shalomich <shalo6947@gmail.com>
2021-05-08 17:43:57 +0700

    create class LogoutHandler

---
+++ DocParty/RequestHandlers/Logout/LogoutHandler.cs
@@ -0,0 +1,27 @@
+﻿using DocParty.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.Logout
+{
+    class LogoutHandler : IRequestHandler<LogoutQuery>
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public LogoutHandler(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+
+        public async Task<Unit> Handle(LogoutQuery request, CancellationToken cancellationToken)
+        {
+            await _signInManager.SignOutAsync();
+            return Unit.Value;
+        }
+    }
+}
