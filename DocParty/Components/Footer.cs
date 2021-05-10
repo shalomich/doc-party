b10b90cb526fdad95d8f07ea706902e1e6ce361d
shalomich <shalo6947@gmail.com>
2021-05-10 18:45:43 +0700

    create component Footer

---
+++ DocParty/Components/Footer.cs
@@ -0,0 +1,30 @@
+﻿using DocParty.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Components
+{
+    public class Footer : ViewComponent
+    {
+        private const string FeedbackConfigPath = "Feedback";
+        private IConfigurationSection FeedbackConfigSection { get; }
+        public Footer(IConfiguration configuration)
+        {
+            FeedbackConfigSection = configuration.GetSection(FeedbackConfigPath);
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var info = new FooterInfo
+            {
+                Email = FeedbackConfigSection["Email"],
+                PhoneNumber = FeedbackConfigSection["PhoneNumber"]
+            };
+            return View(info); 
+        }
+    }
+}
