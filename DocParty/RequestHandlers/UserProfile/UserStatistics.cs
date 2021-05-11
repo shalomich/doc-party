304fc4c78a830e3e9758e67618cab0d48ce37fd4
shalomich <shalo6947@gmail.com>
2021-05-11 13:32:10 +0700

    create class ProfileHandler

---
+++ DocParty/RequestHandlers/UserProfile/UserStatistics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.UserProfile
+{
+    public class UserStatistics
+    {
+        public IEnumerable<(string metricName, int metricValue)> Data { set; get; }
+    }
+}
