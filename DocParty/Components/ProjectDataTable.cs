5504e95a0827b3fd077bafd9bacc4fac9288fe95
shalomich <shalo6947@gmail.com>
2021-05-13 00:40:39 +0700

    create component ProjectsDataTable

---
+++ DocParty/Components/ProjectDataTable.cs
@@ -0,0 +1,39 @@
+﻿
+using DocParty.RequestHandlers.Projects;
+using DocParty.Services;
+using DocParty.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Components
+{
+    public class ProjectDataTable : ViewComponent
+    {
+        private const string ProjectLocation = "{userName}/{projectName}";
+        
+        private readonly Table<ProjectData> _table;
+
+        public ProjectDataTable(Table<ProjectData> table)
+        {
+            _table = table ?? throw new ArgumentNullException(nameof(table));
+        }
+
+        public IViewComponentResult Invoke(ProjectsInfo info)
+        {
+            _table.Fill(info.Data);
+
+            Dictionary<string,string> projectsLocations = info.Data.ToDictionary
+            (
+                projectData => projectData.ProjectName,
+                projectData => String.Format(ProjectLocation, info.UserName, projectData.ProjectName)
+            );
+
+            return View(new ProjectDataTableInfo { Table = _table, ProjectsLocations = projectsLocations});
+        }
+
+
+    }
+}
