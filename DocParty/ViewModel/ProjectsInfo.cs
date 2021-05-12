4865e750e9feead463bb64e9d45484dea58ef3e7
shalomich <shalo6947@gmail.com>
2021-05-13 00:38:45 +0700

    add method ShowProjects in UserController

---
+++ DocParty/ViewModel/ProjectsInfo.cs
@@ -0,0 +1,14 @@
+﻿using DocParty.RequestHandlers.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.ViewModel
+{
+    public class ProjectsInfo
+    {
+        public IEnumerable<ProjectData> Data { set; get; } 
+        public string UserName { set; get; }
+    }
+}
