a446e0ffcd2dad6aa4749b156102c36a375ab0c7
shalomich <shalo6947@gmail.com>
2021-05-13 00:37:13 +0700

    create class ShowProjectsHandler

---
+++ DocParty/RequestHandlers/Projects/ProjectData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.Projects
+{
+    public interface IProjectData
+    {
+        public string ProjectName { set; get; }
+    }
+    public class ProjectData : IProjectData
+    {
+        public string ProjectName { set; get; }
+        public string CreatorName { set; get; }
+        public int AuthorCount { set; get; }
+        public int SnapshotCount { set; get; }
+        public bool IsActive { set; get; }
+    }
+}
---
+++ DocParty/RequestHandlers/Projects/ShowProjectsHandler.cs
@@ -0,0 +1,38 @@
+﻿using DocParty.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.Projects
+{
+    class ShowProjectsHandler : IRequestHandler<UserQuery<IEnumerable<ProjectData>>,IEnumerable<ProjectData>>
+    {
+        private ApplicationContext Context { get; }
+
+        public ShowProjectsHandler(ApplicationContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<IEnumerable<ProjectData>> Handle(UserQuery<IEnumerable<ProjectData>> request, CancellationToken cancellationToken)
+        {
+            return await Context.Projects
+                                   .Where(project => project.AuthorRoles.Any(authorRole => authorRole.User.UserName == request.UserName))
+                                   .Select(project => new ProjectData
+                                   {
+                                       ProjectName = project.Name,
+                                       CreatorName = project.Creator.UserName,
+                                       AuthorCount = project.AuthorRoles.Count(),
+                                       IsActive = project.isActive,
+                                       SnapshotCount = project.Snapshots.Count()
+                                   })
+                                   .ToListAsync();
+        }
+    }
+
+
+}
