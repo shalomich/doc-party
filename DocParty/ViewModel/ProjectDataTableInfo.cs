10d4856a843e0a099cc0a670f64975e5d8a5653a
shalomich <shalo6947@gmail.com>
2021-05-13 00:40:39 +0700

    create component ProjectsDataTable

---
+++ DocParty/ViewModel/ProjectDataTableInfo.cs
@@ -0,0 +1,15 @@
+﻿using DocParty.RequestHandlers.Projects;
+using DocParty.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.ViewModel
+{
+    public class ProjectDataTableInfo
+    {
+        public Table<ProjectData> Table { set; get; }
+        public Dictionary<string, string> ProjectsLocations { set; get; }
+    }
+}
