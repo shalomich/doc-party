0793d1f33517c1c793bd6147e4bf985d99c062cf
shalomich <shalo6947@gmail.com>
2021-05-15 00:12:56 +0700

    create class AddProjectHandler

---
+++ DocParty/RequestHandlers/AddProject/AddingFormData.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.AddProject
+{
+    public class AddingFormData
+    {
+        public string ProjectName { set; get; }
+        public string Description { set; get; }
+    }
+}
