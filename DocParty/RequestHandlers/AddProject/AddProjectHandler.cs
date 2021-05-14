ed178c0450fc23967dbffe31ce0b5ae48476ba41
shalomich <shalo6947@gmail.com>
2021-05-15 00:12:56 +0700

    create class AddProjectHandler

---
+++ DocParty/RequestHandlers/AddProject/AddProjectHandler.cs
@@ -0,0 +1,47 @@
+﻿using DocParty.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocParty.RequestHandlers.AddProject
+{
+    class AddProjectHandler : IRequestHandler<UserHandlerData<AddingFormData,ErrorResponce>, ErrorResponce>
+    {
+        private const string InvalidProjectName = "This project name is belongs to your other project";
+        private ApplicationContext Context { get; }
+
+        public AddProjectHandler(ApplicationContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        
+        public async Task<ErrorResponce> Handle(UserHandlerData<AddingFormData, ErrorResponce> request, CancellationToken cancellationToken)
+        {
+            var errors = new List<string>();
+
+            bool isExist = Context.Projects
+                .Where(project => project.Creator.UserName == request.UserRequest.UserName)
+                .Any(project => project.Name == request.Data.ProjectName);
+
+            if (isExist)
+            {
+                errors.Add(InvalidProjectName);
+                return new ErrorResponce(errors);
+            }
+            User user = await Context.Users.FirstOrDefaultAsync(user => user.UserName == request.UserRequest.UserName);
+
+            var project = new Project(request.Data.ProjectName, request.Data.Description, user);
+
+            await Context.Projects.AddAsync(project);
+
+            Context.SaveChanges();
+
+            return new ErrorResponce(errors);
+
+        }
+    }
+}
