be9fbed4f8050dd2ae4ac3643c94c87f01fd1621
shalomich <shalo6947@gmail.com>
2021-05-15 23:43:54 +0700

    create class TableDecorator

---
+++ DocParty/Services/Tables/TableDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Services.Tables
+{
+    public abstract class TableDecorator : ITable
+    {
+        protected ITable _table;
+        protected TableDecorator(ITable table)
+        {
+            _table = table ?? throw new ArgumentNullException(nameof(table));
+        }
+
+        public abstract string[] ColumnNames { get; }
+        public abstract string[,] Values { get; }
+    }
+}
