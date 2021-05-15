98482d823626def9d54f554cf2549e425b4b34fc
shalomich <shalo6947@gmail.com>
2021-05-15 23:41:27 +0700

    create interface ITable

---
+++ DocParty/Services/Tables/ITable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Services.Tables
+{
+    public interface ITable
+    {
+        public string[] ColumnNames { get; }        
+        public string[,] Values { get; }
+        public int RowCount => Values.Length / ColumnNames.Length;
+    }
+}
