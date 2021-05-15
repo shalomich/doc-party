c4d323de33f55a781f504e8fdc0e89cbcd776724
shalomich <shalo6947@gmail.com>
2021-05-16 00:11:38 +0700

    create class ShowSnapshotsHandler

---
+++ DocParty/RequestHandlers/ShowSnapshots/ShowSnapshotsHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.ShowSnapshots
+{
+    class ShowSnapshotsHandler : IRequestHandler<HandlerData<UserRequest, IEnumerable<SnapshotData>>, IEnumerable<SnapshotData>>
+    {
+        private ApplicationContext Context { get; }
+        public ShowSnapshotsHandler(ApplicationContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<IEnumerable<SnapshotData>> Handle(HandlerData<UserRequest, IEnumerable<SnapshotData>> request, CancellationToken cancellationToken)
+        {
+            return Context.ProjectShapshots
+                .Where(snapshot => snapshot.Author.UserName == request.Data.UserName)
+                .Select(snapshot => new SnapshotData
+                {
+                    SnapshotName = snapshot.Name,
+                    ProjectName = snapshot.Project.Name,
+                    CommentCount = snapshot.Comments.Count(),
+                    Description = snapshot.Description
+                }).ToList();
+        }
+    }
+}
---
+++ DocParty/RequestHandlers/ShowSnapshots/SnapshotData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.ShowSnapshots
+{
+    public class SnapshotData
+    {
+        public string SnapshotName { set; get; }
+        public string ProjectName { set; get; }
+        public int CommentCount { set; get; }
+        public string Description { set; get; }
+    }
+}
