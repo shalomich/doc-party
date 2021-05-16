e76e1c0726ac56cc326995c574894bb3e002f34e
shalomich <shalo6947@gmail.com>
2021-05-17 00:03:04 +0700

    add methods for project page work

--- DocParty/Controllers/ProjectController.cs
+++ DocParty/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
-﻿using DocParty.RequestHandlers;
+﻿using DocParty.Models;
+using DocParty.RequestHandlers;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -20,9 +21,37 @@ namespace DocParty.Controllers
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
+        public async Task<IActionResult> Show([FromRoute] string userName, [FromRoute] string projectName)
+        {
+            var request = new HandlerData<ProjectRequest, Project>
+            {
+                Data = new ProjectRequest { UserName = userName, ProjectName = projectName },
+            };
+
+            Project project = await _mediator.Send(request);
+
+            return View("Project", project);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddSnapshot([FromRoute] string userName, [FromRoute] string projectName, [FromForm] SnapshotFormData formData)
+        {
+            string currentUserName = User.Identity.Name;
+
+            var request = new UserHandlerData<SnapshotFormData, ErrorResponce>
+            {
+                UserRequest = new ProjectRequest { UserName = currentUserName, ProjectName = projectName },
+                Data = formData
+            };
+
+            ErrorResponce responce = await _mediator.Send(request);
+
+            return RedirectToAction(nameof(Show), new { userName = userName, projectName = projectName });
+        }
+
         [HttpPost]
         [Route("deletion")]
-        public async Task<IActionResult> DeleteProject([FromRoute] string userName, [FromRoute] string projectName)
+        public async Task<IActionResult> Delete([FromRoute] string userName, [FromRoute] string projectName)
         {
             var request = new HandlerData<ProjectRequest, ErrorResponce>
             {
@@ -31,7 +60,22 @@ namespace DocParty.Controllers
 
             ErrorResponce responce = await _mediator.Send(request);
 
-            return RedirectToRoute("projects",new {userName = userName});
+            return RedirectToRoute("Projects",new {userName = userName});
+        }
+
+        [HttpPost]
+        [Route("state")]
+        
+        public async Task<IActionResult> ChangeState([FromRoute] string userName, [FromRoute] string projectName)
+        {
+            var request = new HandlerData<ProjectRequest, Unit>
+            {
+                Data = new ProjectRequest { UserName = userName, ProjectName = projectName },
+            };
+
+            await _mediator.Send(request);
+
+            return RedirectToAction(nameof(Show), new { userName = userName, projectName = projectName});
         }
     }
 }
