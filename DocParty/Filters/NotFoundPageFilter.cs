228b5f01d25e7b5d5736d57370c705d57a22815c
shalomich <shalo6947@gmail.com>
2021-05-22 23:50:28 +0700

    create class NotFoundPageFilter

---
+++ DocParty/Filters/NotFoundPageFilter.cs
@@ -0,0 +1,21 @@
+﻿using DocParty.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DocParty.Filters
+{
+    public class NotFoundPageFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is NotFoundException)
+                context.Result = new NotFoundResult();
+            context.ExceptionHandled = true;
+        }
+    }
+}
