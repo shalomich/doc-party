d47dda9ab93fa3a54842d55943efec590242056b
shalomich <shalo6947@gmail.com>
2021-05-24 01:04:24 +0700

    create class SetTempDataModelStateAttribute

---
+++ DocParty/Filters/SetTempDataModelStateAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DocParty.Filters
+{
+    public class SetTempDataModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            var controller = filterContext.Controller as Controller;
+
+            var errors = new List<string>();
+            
+            foreach (var value in controller.ViewData.ModelState.Values)
+                foreach (var error in value.Errors)
+                    errors.Add(error.ErrorMessage);
+
+            controller.TempData["Errors"] = JsonConvert.SerializeObject(errors);
+        }
+    }
+}
