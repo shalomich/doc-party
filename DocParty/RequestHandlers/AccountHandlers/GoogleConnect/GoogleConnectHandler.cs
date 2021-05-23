ba8a7fbfe002e12a64df3b34e363fa8d04bbbfa2
shalomich <shalo6947@gmail.com>
2021-05-23 19:07:04 +0700

    create class GoogleConnectHandler

---
+++ DocParty/RequestHandlers/AccountHandlers/GoogleConnect/GoogleConnectHandler.cs
@@ -0,0 +1,28 @@
+﻿using DocParty.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DocParty.RequestHandlers.AccountHandlers.GoogleConnect
+{
+    class GoogleConnectHandler : IRequestHandler<HandlerData<string, AuthenticationProperties>,AuthenticationProperties>
+    {
+        private SignInManager<User> SignInManager { get; }
+
+        public GoogleConnectHandler(SignInManager<User> signInManager)
+        {
+            SignInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+
+        public Task<AuthenticationProperties> Handle(HandlerData<string, AuthenticationProperties> request, CancellationToken cancellationToken)
+        {
+            string redirectUrl = request.Data;
+            return Task.FromResult(SignInManager.ConfigureExternalAuthenticationProperties("Google", redirectUrl));
+        }
+    }
+}
