991fa954874dc8a5b73e32e96680d71cf0b496ab
shalomich <shalo6947@gmail.com>
2021-05-23 15:15:11 +0700

    configure class Startup for google auth

---
+++ DocParty/Extensions/StringBuilderExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocParty.Extensions
+{
+    public static class StringBuilderExtension
+    {
+        public static StringBuilder GetRussianSymbols(this StringBuilder builder)
+        {
+            int russianAlphabetLength = 32;
+            for (int i = 0; i < russianAlphabetLength; i++)
+            {
+                builder.Append((char)('а' + i));
+                builder.Append((char)('А' + i));
+            }
+
+            return builder;
+        }
+    }
+}
