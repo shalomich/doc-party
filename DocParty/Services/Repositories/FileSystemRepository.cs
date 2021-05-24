6853de2d51e87b59bd07a5fdc687efb392cdfd84
shalomich <shalo6947@gmail.com>
2021-05-25 01:10:54 +0700

    create class FileSystemRepository

---
+++ DocParty/Services/Repositories/FileSystemRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Services.Repositories
+{
+    class FileSystemRepository : IRepository<byte[], string>
+    {
+        private readonly string RooteFilePath;
+        public FileSystemRepository(IWebHostEnvironment environment)
+        {
+            RooteFilePath = environment.WebRootPath + "/Files/";       
+        }
+
+        private string GetFilePath(string fileName)
+        {
+            return $"{RooteFilePath}/{fileName}";
+        }
+
+        public void Create(string id, byte[] fileBytes)
+        {
+            using var stream = new FileStream(GetFilePath(id), FileMode.Create);
+            stream.Write(fileBytes);
+        }
+
+        public void Delete(string id)
+        {
+            File.Delete(GetFilePath(id));
+        }
+
+        public byte[] Select(string id)
+        {
+            using var stream = new FileStream(GetFilePath(id), FileMode.Open);
+            byte[] fileBytes = new byte[stream.Length];
+            
+            stream.Read(fileBytes, 0, fileBytes.Length);
+            
+            return fileBytes;
+        }
+
+        public void Update(string id, byte[] fileBytes)
+        {
+            using var stream = new FileStream(GetFilePath(id), FileMode.OpenOrCreate);
+            stream.Write(fileBytes);
+        }
+    }
+}
