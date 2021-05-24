a1d2d0c08d44ac125346862545294c64b8458827
shalomich <shalo6947@gmail.com>
2021-05-25 01:09:02 +0700

    create interface IRepository

---
+++ DocParty/Services/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Services.Repositories
+{
+    public interface IRepository<T,K>
+    {
+        T Select(K id);
+        void Create(K id,T obj);
+        void Update(K id,T obj);
+        void Delete(K id);
+    }
+}
