2bda960c1be62976806a852d6e7df078355388a0
shalomich <shalo6947@gmail.com>
2021-05-25 18:04:04 +0700

    write setting for aws s3 in config

---
+++ DocParty/Services/AwsS3FileRepository.cs
@@ -0,0 +1,37 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Services
+{
+    class AwsS3FileRepository
+    {
+        public async Task Add(IFormFile file)
+        {
+            using (var client = new AmazonS3Client("yourAwsAccessKeyId", "yourAwsSecretAccessKey", RegionEndpoint.USEast1))
+            {
+                using (var newMemoryStream = new MemoryStream())
+                {
+                    file.CopyTo(newMemoryStream);
+
+                    var uploadRequest = new TransferUtilityUploadRequest
+                    {
+                        InputStream = newMemoryStream,
+                        Key = file.FileName,
+                        BucketName = "yourBucketName",
+                        CannedACL = S3CannedACL.PublicRead
+                    };
+
+                    var fileTransferUtility = new TransferUtility(client);
+                    await fileTransferUtility.UploadAsync(uploadRequest);
+                }
+            }
+        }
+    }
+}
