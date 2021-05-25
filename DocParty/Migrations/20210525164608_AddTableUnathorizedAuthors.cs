c3386757b6443981975b00b76d7c73b8826b8831
shalomich <shalo6947@gmail.com>
2021-05-25 23:48:25 +0700

    create class UnauthorizedAuthor

---
+++ DocParty/Migrations/20210525164608_AddTableUnathorizedAuthors.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DocParty.Migrations
+{
+    public partial class AddTableUnathorizedAuthors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnauthorizedAuthors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnauthorizedAuthors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UnauthorizedAuthors_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnauthorizedAuthors_ProjectId",
+                table: "UnauthorizedAuthors",
+                column: "ProjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnauthorizedAuthors");
+        }
+    }
+}
---
+++ DocParty/Models/UnauthorizedAuthor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Models
+{
+    public class UnauthorizedAuthor
+    {
+        public int Id { set; get; }
+        public string Email { set; get; }
+
+        public Project Project { set; get; }
+        public int ProjectId { set; get; }
+    }
+}
