45ae62bb3ccf76069e02ffa6ad95fde327f4c7c9
shalomich <shalo6947@gmail.com>
2021-05-26 01:06:14 +0700

    create class EmailSender

---
+++ DocParty/Services/Email/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Services.Email
+{
+    class EmailSender
+    {
+        private const string FeedbackConfigSectionName = "Feedback";
+        private readonly string Email;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            Email = configuration.GetSection(FeedbackConfigSectionName)[nameof(Email)];
+        }
+
+        public async Task SendEmailAsync(string recipientEmail, EmailMessage message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("DocParty", Email));
+            emailMessage.To.Add(new MailboxAddress("", recipientEmail));
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = message.Text
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
