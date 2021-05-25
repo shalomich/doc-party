2b30ba3fab5ad0e45ba46f4371d679dfc3fce857
shalomich <shalo6947@gmail.com>
2021-05-26 01:05:38 +0700

    create class EmailMessage

---
+++ DocParty/Services/Email/EmailMessageCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocParty.Services.Email
+{
+    class EmailMessageCreator
+    {
+        private EmailMessageBuilder Builder { set; get; }
+        public EmailMessageCreator(EmailMessageBuilder builder)
+        {
+            Builder = builder ?? throw new ArgumentNullException(nameof(builder));
+        }
+
+        public EmailMessage Create()
+        {
+            Builder.SetText();
+            Builder.SetSubject();
+            return Builder.Build();
+        }
+    }
+}
